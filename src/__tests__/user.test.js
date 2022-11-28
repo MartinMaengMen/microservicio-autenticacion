02864a495b17affc95b88c06e09ae31a2e9520cd
Martín <u201520759@upc.edu.pe>
2022-11-28 18:57:49 -0500

    Se agregaron test

--- src/__tests__/user.test.js
+++ src/__tests__/user.test.js
@@ -1,15 +1,37 @@
-const { describe } = require('node:test')
 const {loginService, registerService} = require('../services/user.service')
+const request = require('supertest')
 
-const user = {
-
-}
+const requestLogin = request('https://1xvsj3adyf.execute-api.sa-east-1.amazonaws.com')
 
 describe('user',()=>{
     describe('user login',()=>{
-        describe('given email or password does not match',()=>{
-            it('should return a 404', async()=>{
-                
+        const postLogin = requestLogin.post('/login')
+        describe('given user is registered',()=>{
+            const userLogin = {
+                "email":"martin.ng@nttdata.com",
+                "password":"martin.ng"
+            }
+            it('should return status code 200', async()=>{
+                const {statusCode} = await postLogin.send(userLogin)
+                expect(statusCode).toBe(200)
+            })
+            it('should return an object', async()=>{
+                const {body} = await postLogin.send(userLogin)
+                expect(body).toBeInstanceOf(Object)
+            })
+        })
+        describe('given email does not match',()=>{
+            const userLogin = {
+                "email":"martinng@nttdata.com",
+                "password":"martin.ng"
+            }
+            it('should return status code 404', async()=>{
+                const {statusCode} = await postLogin.send(userLogin)
+                expect(statusCode).toBe(404)
+            })
+            it('should not return a body', async()=>{
+                const {body} = await postLogin.send(userLogin)
+                expect(body).toBeInstanceOf(null)
             })
         })
     })
