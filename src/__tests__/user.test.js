aba253d3342aa9927872c2ee053c96e6ddf60343
Martín <u201520759@upc.edu.pe>
2022-11-29 01:21:22 -0500

    se agregaron los test para registrar y login

--- src/__tests__/user.test.js
+++ src/__tests__/user.test.js
@@ -1,28 +1,43 @@
 const request = require('supertest')
-const {loginService,registerService} = require('../services/user.service')
+const userService = require('../services/user.service')
 
 const requestLogin = request('https://1xvsj3adyf.execute-api.sa-east-1.amazonaws.com')
 
 describe('user',()=>{
     describe('user login',()=>{
-        const postLogin = requestLogin.post('/login')
+        describe('given email does not registered',()=>{
+            const userLogin = {
+                "email":"martinng@nttdata.com",
+                "password":"martin.ng"
+            }
+            it('should return status code 404', async()=>{
+                const {statusCode} = await requestLogin.post('/login').send(userLogin)
+                expect(statusCode).toBe(404)
+            })
+        })
+        describe('given user does not match',()=>{
+            const userLogin = {
+                "email":"martin.ng@nttdata.com",
+                "password":"abcds"
+            }
+            it('should return status code 403', async()=>{
+                const {statusCode} = await requestLogin.post('/login').send(userLogin)
+                expect(statusCode).toBe(403)
+            })
+        })
         describe('given user is registered',()=>{
             const userLogin = {
                 "email":"martin.ng@nttdata.com",
                 "password":"martin.ng"
             }
             it('should return status code 200', async()=>{
-                const {statusCode} = await postLogin.send(userLogin)
+                const {statusCode,body} = await requestLogin.post('/login').send(userLogin)
                 expect(statusCode).toBe(200)
-            })
-            it('should return an object', async()=>{
-                const {body} = await postLogin.send(userLogin)
                 expect(body).toBeInstanceOf(Object)
             })
         })
     })
     describe('user register',()=>{
-        const postLogin = requestLogin.post('/register')
         describe('given email already exists',()=>{
             const userRegister = {
                 "email":"martin.ng@nttdata.com",
@@ -32,10 +47,37 @@ describe('user',()=>{
                 "surname":"efg"
             }
             it('should return status code 400', async()=>{
-                const {statusCode} = await postLogin.send(userRegister)
+                const {statusCode} = await requestLogin.post('/register').send(userRegister)
+                expect(statusCode).toBe(400)
+            })
+        })
+        describe('given email is not correct',()=>{
+            const userRegister = {
+                "email":"martin.ng@nttda",
+                "password":"abcd1234#",
+                "dni":56738492,
+                "name":"abcd",
+                "surname":"efg"
+            }
+            it('should return status code 400', async()=>{
+                const {statusCode} = await requestLogin.post('/register').send(userRegister)
+                expect(statusCode).toBe(400)
+            })
+        })
+        describe('given dni is not correct',()=>{
+            const userRegister = {
+                "email":"martin.ng@nttdata.com",
+                "password":"abcd1234#",
+                "dni":123,
+                "name":"abcd",
+                "surname":"efg"
+            }
+            it('should return status code 400', async()=>{
+                const {statusCode} = await requestLogin.post('/register').send(userRegister)
                 expect(statusCode).toBe(400)
             })
         })
     })
+    
 })
 
--- src/utils/validations.handle.js
+++ src/utils/validations.handle.js
@@ -2,10 +2,6 @@ const validateEmail = (email) => {
     var re = /\S+@\S+\.\S+/;
     return re.test(email);
   }
-const validatePassword = (password) => {
-    var re = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
-    return re.test(password);
-  }
 const validateDNI = (dni)=>{
   var re = /^[0-9]{8}$/;
   return re.test(dni)
@@ -13,9 +9,6 @@ const validateDNI = (dni)=>{
 const validateParameters = (email,password,dni)=>{
     if(!validateEmail(email)){
       return 'Correo no valido'
-    }
-    if(!validatePassword(password)){
-      return 'La contraseña debe de contener al menos 6 caracteres, un número y un caracter especial'
     }
     if(!validateDNI(dni)){
       return 'DNI no valido'
